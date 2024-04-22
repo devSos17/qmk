1257fde3dd3edb1856d9f2065c285767792399ab
Santiago Orozco <devsos117@gmail.com>
2024-04-22 00:15:54 -0600

    Add leds

--- keyboards/sofle/keymaps/s1/extra.c
+++ keyboards/sofle/keymaps/s1/extra.c
@@ -12,12 +12,12 @@ enum sofle_layers {
 /* KEYBOARD PET START */
 
 /* settings */
-#    define MIN_WALK_SPEED      10
-#    define MIN_RUN_SPEED       40
+#define MIN_WALK_SPEED      10
+#define MIN_RUN_SPEED       40
 
 /* advanced settings */
-#    define ANIM_FRAME_DURATION 200  // how long each frame lasts in ms
-#    define ANIM_SIZE           96   // number of bytes in array. If you change sprites, minimize for adequate firmware size. max is 1024
+#define ANIM_FRAME_DURATION 200  // how long each frame lasts in ms
+#define ANIM_SIZE           96   // number of bytes in array. If you change sprites, minimize for adequate firmware size. max is 1024
 
 /* timers */
 uint32_t anim_timer = 0;
